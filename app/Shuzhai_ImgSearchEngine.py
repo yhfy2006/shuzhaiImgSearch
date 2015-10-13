6487c01969e26902d1d42ac9b70c22a8052ed925
chhe <chhe@ebay.com>
2015-10-13 18:31:39 -0400

    commit

--- app/Shuzhai_ImgSearchEngine.py
+++ app/Shuzhai_ImgSearchEngine.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 # -*- coding:utf-8 -*-
 from flask import Flask
-from flask import jsonify
+from flask import request
 import requests
 import json
 from flask import render_template
@@ -10,7 +10,7 @@ app = Flask(__name__)
 
 defalutResultNumber = 10
 queryText = ''
-imgSearchUrl = "http://image.baidu.com/i?tn=baiduimagejson&ct=201326592&cl=2&lm=-1&st=-1&fm=result&fr=&sf=1&fmq=1349413075627_R&pv=&ic=0&nc=1&z=&se=1&showtab=0&fb=0&width=&height=&face=0&istype=2&rn="+str(defalutResultNumber)+"&pn=1&ie=utf-8&word="
+imgSearchUrl = "http://image.baidu.com/i?tn=baiduimagejson&ct=201326592&cl=2&lm=-1&st=-1&fm=result&fr=&sf=1&fmq=1349413075627_R&pv=&ic=0&nc=1&z=&se=1&showtab=0&fb=0&width=&height=&face=0&istype=2&rn=%s&pn=1&ie=utf-8&word=%s"
 
 @app.route('/')
 def hello_world():
@@ -18,7 +18,12 @@ def hello_world():
 
 @app.route('/query/<queryText>')
 def queryText(queryText):
-    finalSearchURL = imgSearchUrl+queryText
+    rn = request.args.get('rn')
+    finalSearchURL= ''
+    if rn is not None:
+        finalSearchURL = imgSearchUrl %(rn,queryText)
+    else:
+        finalSearchURL = imgSearchUrl %(defalutResultNumber,queryText)
     r = requests.get(finalSearchURL)
     d = json.loads(r.text)
     listingArray = d['data']
